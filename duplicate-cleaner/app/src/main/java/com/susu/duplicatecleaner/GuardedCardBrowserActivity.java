fbe87f5de8514b12f93efe05d4d1d7a5aad4b9fd
ssu26514-lab <ssu26514@gmail.com>
2026-07-13 23:36:08 +0800

    Guard card browser with independent session lock

---
+++ duplicate-cleaner/app/src/main/java/com/susu/duplicatecleaner/GuardedCardBrowserActivity.java
@@ -0,0 +1,24 @@
+package com.susu.duplicatecleaner;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class GuardedCardBrowserActivity extends CardBrowserActivity {
+    private boolean sessionAcquired;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        sessionAcquired = ToolSession.acquire(ToolSession.Mode.BROWSER);
+        super.onCreate(savedInstanceState);
+        if (!sessionAcquired) {
+            Toast.makeText(this, "另一个文件工具仍在运行，请先退出后再使用。", Toast.LENGTH_LONG).show();
+            finish();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (sessionAcquired) ToolSession.release(ToolSession.Mode.BROWSER);
+        super.onDestroy();
+    }
+}
